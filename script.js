e1337d7782895422cd3a7c8caa4c042727cbd618
syafiqhost <syafiqwebsite@gmail.com>
2024-12-23 16:30:19 +0700

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const messages = [
     "Network connection: Wi-Fi (Strong)",
     "IP ADRESS: 209.332.108.908",
     "NUMBER PHONE : +62-877-7554-****",
-    "Location device : https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126791.12193777908!2d108.4716355398652!3d-6.7426852042482945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ee2649e6e5bbb%3A0x70a07638a7fe12fe!2sCirebon%2C%20Kota%20Cirebon%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1734944839204!5m2!1sid!2sid",
+    "Device location: https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126791.12193777908!2d108.4716355398652!3d-6.7426852042482945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ee2649e6e5bbb%3A0x70a07638a7fe12fe!2sCirebon%2C%20Kota%20Cirebon%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1734944839204!5m2!1sid!2sid",
     "Scanning for unauthorized access attempts...",
     "Alert: System integrity compromised.",
 ];
